903df09e91262cbfcef227544a4351c7e0866a1f
james890311 <55519698+james890311@users.noreply.github.com>
2020-01-09 14:00:58 +0800

    0109

---
+++ Unity_VR_B1040402_ZHELUN/Assets/coin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class coin : MonoBehaviour
+{
+
+    public int countCoin;
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "平台")
+        {
+            Destroy(gameObject);
+            countCoin += 1;
+        }
+        
+    }
+    
+}
---
+++ Unity_VR_B1040402_ZHELUN/Assets/coincount.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class concount : MonoBehaviour
+{
+    public Text trashcount;
+    public static int LostTrash = 12;
+
+    private void Update()
+    {
+        trashcount.text = "金幣剩餘量：" + LostTrash;
+    }
+}
